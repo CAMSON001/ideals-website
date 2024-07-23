f1136c5007be22700b0a287828cc234a49175708
SYLLA <synf30@gmail.com>
2024-07-23 10:52:16 +0100

    Debug

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; 
+import ReactDOM from 'react-dom/client'; 
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 root.render(
   <HelmetProvider>
